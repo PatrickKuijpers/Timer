0c08e7be4e423f294844607d2ea33428b2b6f672
Patrick Kuijpers <patrickuijpers@gmail.com>
2017-01-08 21:51:46 +0100

    Clicks zorgen voor ophalen & tonen van de tijd

--- mobile/src/main/java/nl/tcilegnar/timer/views/DayEditorItemView.java
+++ mobile/src/main/java/nl/tcilegnar/timer/views/DayEditorItemView.java
@@ -1,18 +1,25 @@
 package nl.tcilegnar.timer.views;
 
 import android.content.Context;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Locale;
 
 import nl.tcilegnar.timer.R;
 import nl.tcilegnar.timer.enums.DayEditorItem;
 
-public class DayEditorItemView extends LinearLayout {
+import static android.view.View.OnClickListener;
+
+public class DayEditorItemView extends LinearLayout implements OnClickListener {
 
     private final DayEditorItem dayEditorItem;
 
     private TextView label;
     private TextView value;
+    private Calendar currentTime;
 
     public DayEditorItemView(Context activityContext, DayEditorItem dayEditorItem) {
         super(activityContext);
@@ -26,10 +33,34 @@ public class DayEditorItemView extends LinearLayout {
     private void initViews() {
         label = (TextView) findViewById(R.id.day_editor_item_label);
         value = (TextView) findViewById(R.id.day_editor_item_value);
+
+        setOnClickListener(this);
     }
 
     private void initValues(DayEditorItem dayEditorItem) {
         label.setText(dayEditorItem.name());
-        value.setText("00:00");
+        value.setText("..:..");
+    }
+
+    @Override
+    public void onClick(View view) {
+        currentTime = getCurrentTime();
+        setCurrentTimeText(currentTime);
+    }
+
+    private Calendar getCurrentTime() {
+        Locale locale = Locale.GERMANY;
+        return Calendar.getInstance(locale);
+    }
+
+    private void setCurrentTimeText(Calendar currentTime) {
+        String currentTimeText = formatToCurrentTimeText(currentTime);
+        value.setText(currentTimeText);
+    }
+
+    private String formatToCurrentTimeText(Calendar currentTime) {
+        int minute = currentTime.get(Calendar.MINUTE);
+        int hour = currentTime.get(Calendar.HOUR_OF_DAY);
+        return hour + ":" + minute;
     }
 }
