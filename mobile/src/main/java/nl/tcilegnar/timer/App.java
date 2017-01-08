1a59df7479c7045377dfd03f2c91fb4af8c6c4b1
Patrick Kuijpers <patrickuijpers@gmail.com>
2017-01-08 18:01:07 +0100

    Voorkom overschreiden dex limiet

--- mobile/src/main/java/nl/tcilegnar/timer/App.java
+++ mobile/src/main/java/nl/tcilegnar/timer/App.java
@@ -9,7 +9,6 @@ import android.support.annotation.ColorRes;
 import android.support.annotation.DimenRes;
 import android.support.annotation.IntegerRes;
 import android.support.annotation.StringRes;
-import android.support.multidex.MultiDex;
 import android.support.v4.content.ContextCompat;
 
 public class App extends Application {
@@ -49,31 +48,5 @@ public class App extends Application {
     @ColorInt
     int getResourceColor(@ColorRes int resId) {
         return ContextCompat.getColor(context, resId);
-    }
-
-    /** stackoverflow.com/questions/37998266/android-studio-many-error-could-not-find-class-android-xxx#38666791 */
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-
-        try {
-            MultiDex.install(this);
-        } catch (RuntimeException multiDexException) {
-            // Work around Robolectric causing multi dex installation to fail, see
-            // https://code.google.com/p/android/issues/detail?id=82007.
-            boolean isUnderUnitTest;
-
-            try {
-                Class<?> robolectric = Class.forName("org.robolectric.Robolectric");
-                isUnderUnitTest = (robolectric != null);
-            } catch (ClassNotFoundException e) {
-                isUnderUnitTest = false;
-            }
-
-            if (!isUnderUnitTest) {
-                // Re-throw if this does not seem to be triggered by Robolectric.
-                throw multiDexException;
-            }
-        }
     }
 }
