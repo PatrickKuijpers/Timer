7ed210cf7fd7d6ba1f5a91f0059a5c83f477c024
Patrick Kuijpers <patrickuijpers@gmail.com>
2017-05-28 22:31:18 +0200

    Total time calculated

--- mobile/src/main/java/nl/tcilegnar/timer/fragments/DayEditorFragment.java
+++ mobile/src/main/java/nl/tcilegnar/timer/fragments/DayEditorFragment.java
@@ -8,17 +8,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 import nl.tcilegnar.timer.R;
 import nl.tcilegnar.timer.adapters.DayEditorAdapter;
 import nl.tcilegnar.timer.dialogs.TimePickerFragment;
+import nl.tcilegnar.timer.enums.DayEditorItem;
 
 import static nl.tcilegnar.timer.views.DayEditorItemView.TimePickerDialogListener;
 
 public class DayEditorFragment extends Fragment implements TimePickerDialogListener {
-
     private ListView dayEditorList;
     private DayEditorAdapter dayEditorAdapter;
+    private TextView totalValueView;
 
     public DayEditorFragment() {
     }
@@ -32,9 +38,37 @@ public class DayEditorFragment extends Fragment implements TimePickerDialogListe
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         initViews(view);
+        calculateTotalTime();
+    }
+
+    private void calculateTotalTime() {
+        Calendar startTime = DayEditorItem.Start.getCurrentTime();
+        Calendar endTime = DayEditorItem.End.getCurrentTime();
+        long workingDayTime = getDateDiff(startTime, endTime, TimeUnit.MINUTES);
+
+        Calendar breakStartTime = DayEditorItem.BreakStart.getCurrentTime();
+        Calendar breakEndTime = DayEditorItem.BreakEnd.getCurrentTime();
+        long breakTime = getDateDiff(breakStartTime, breakEndTime, TimeUnit.MINUTES);
+
+        long totalTime = workingDayTime - breakTime;
+        String timeString = getReadableTimeString((int) totalTime);
+        totalValueView.setText(timeString);
+    }
+
+    public static long getDateDiff(Calendar cal1, Calendar cal2, TimeUnit timeUnit) {
+        long diffInMillis = cal2.getTimeInMillis() - cal1.getTimeInMillis();
+        return timeUnit.convert(diffInMillis, TimeUnit.MILLISECONDS);
+    }
+
+    public String getReadableTimeString(int timeInMinutes) {
+        int hours = timeInMinutes / 60;
+        int minutes = timeInMinutes % 60;
+        return String.format(Locale.getDefault(), "%d:%02d", hours, minutes);
     }
 
     private void initViews(View view) {
+        totalValueView = (TextView) view.findViewById(R.id.total_value);
+
         dayEditorList = (ListView) view.findViewById(R.id.day_editor_list);
         dayEditorAdapter = new DayEditorAdapter(getActivity(), this);
         dayEditorList.setAdapter(dayEditorAdapter);
